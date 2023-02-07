9cfb665fb30ecad4ed553844306dd36e632d8cb3
p.kamrani <p.kamrani@pikad.com>
2023-02-07 17:09:16 +0330

    convertors month

---
+++ Snap.Core/ViewModels/ChartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snap.Core.ViewModels
+{
+    public class ChartViewModel
+    {
+        public string Type { get; set; }
+        public int? Value { get; set; }
+        public string Color { get; set; }
+    }
+}
